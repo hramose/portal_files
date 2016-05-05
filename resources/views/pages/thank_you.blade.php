5d97346e36dbc8aef35fd7e8afb7b4ab9af81b50
ChibabaChenyuchi <crownsmud@gmail.com>
2016-05-05 21:29:44 +0200

    scripts

--- resources/views/pages/thank_you.blade.php
+++ resources/views/pages/thank_you.blade.php
@@ -1,3 +1,6 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
+
 <script type="text/javascript">
 	$(document).ready(function(){
        window.location.replace("http://www.ngomneestates.co.za/history/");
