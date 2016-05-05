b647bc457eada1612e43822e52ef4db98c65867d
ChibabaChenyuchi <crownsmud@gmail.com>
2016-05-05 23:08:23 +0200

    final push

--- resources/views/pages/thank_you.blade.php
+++ resources/views/pages/thank_you.blade.php
@@ -6,40 +6,22 @@
 
 @section('section')
 	    
-
-	    <div class="col-sm-12">
-						<div class="prophoto">
-							<div class="comment-link">
-								<div class="panel panel-default">
-
-									<div class="panel-heading">
-										<h3 class="panel-title">
-											Thank <br><span class="text-muted">You</span> 
-										</h3>
-									</div>
-									<div class="panel-body">
+<form role="form" method="POST">
+	   {!! csrf_field() !!}
+		<div class="form-content" >
+		    <div class="panel-body">
 										<div class="lorem">
-											Thank you for registering with us. An email has been sent with the next steps you need to make to complete regisrering your will.  
+											Thank you for registering with us. An email has been sent with the next steps you need to make to complete regisrering your will. 
 										</div>
-										
-										<div class="comments-here media">
-											<a class="pull-left" href="#">
-												<img class="media-object img-circle img-responsive" src="images/flat-avatar.png" alt="Media Object">
-											</a>
-											<div class="media-body">
+		</div>
+		</div>
+		<div class="media-body">
 												<a href=""><h5 class="media-heading"></h5></a>
 												<span class="timely pull-right text-muted"></span>
 												Redirecting you to our main site ....
 												<div class="comment-like"><a href=""><i class="fa fa-comments-o"></i></a><a href=""><i class="fa fa-heart text-danger"></i></a></div>
 											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-
-						</div>
-					</div>
-		
+	</form>	
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
 
